eadb8e06c51ab76e89f0523511d2966fe5e48355
HarryShengCS <harryshengcoding@gmail.com>
2024-12-08 16:39:47 +1300

    feat: now only logged-in user can sign up for event

--- controllers/event.js
+++ controllers/event.js
@@ -1,6 +1,7 @@
 const eventsRouter = require('express').Router()
 const Event = require('../models/event')
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 eventsRouter.get('/', async (request, response) => {
   const events = await Event.find({})
@@ -20,16 +21,24 @@ eventsRouter.post('/', async (request, response) => {
   response.status(201).json(savedEvent)
 })
 
+// separate token from authorization header
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 eventsRouter.put('/:eventId/signup', async (request, response) => {
   const {eventId} = request.params
-  const {userId} = request.body
 
-  const user = await User.findById(userId)
-  const event = await Event.findById(eventId)
-
-  if (!user) {
-    return response.status(404).json({ error: 'User not found' })
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+  const user = await User.findById(decodedToken.id)
+  const event = await Event.findById(eventId)
 
   if (!event) {
     return response.status(404).json({ error: 'Event not found' })
