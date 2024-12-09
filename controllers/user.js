c42d7bf83f04278f075ecda63a7e286e07cd4b0a
HarryShengCS <harryshengcoding@gmail.com>
2024-12-10 11:02:50 +1300

    feat: able to retrieve  events that user are joined

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
@@ -23,6 +24,24 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+// separate token from authorization header
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+usersRouter.get('/events', async (request, response) => {
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id).populate('events');
+  response.json(user.events);
 })
 
 module.exports = usersRouter
