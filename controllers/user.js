15ba22fe28be254f30a017bdf6c097f59b6584d5
HarryShengCS <harryshengcoding@gmail.com>
2024-12-10 17:10:58 +1300

    feat: populate user events filed when getting all users

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("events");
   response.json(users);
 });
 
