d21a2e99af5be248ff0ffe076d25bab25434669b
HarryShengCS <harryshengcoding@gmail.com>
2024-12-13 00:13:20 +1300

    refactor: put everything into springboot backend folder so easier to merge

---
+++ SpringBoot-BackEnd/src/main/java/com/nzpmcPlatform/models/User.java
@@ -0,0 +1,41 @@
+package com.nzpmcPlatform.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("user")
+public class User {
+
+    @Id
+    private String email;
+    private String name;
+
+    public User(String email, String name){
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
